97084d3220af60f06c02f6afeefe2db53aae41fc
MChandir <manjula.chandirasekaran@tri-c.edu>
2020-06-01 18:51:26 +0000

    changed 3 to 6

--- main.cs
+++ main.cs
@@ -3,6 +3,6 @@ using System;
 class MainClass {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
-    Console.WriteLine(2+3);
+    Console.WriteLine(2+6);
   }
 }
